3682cc0d28a5d4e0f8dfbdf27d918ecd6cd5e4f4
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-05-22 18:46:25 -0500

    *Update main.js*

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,11 @@
 function SignIn(){
-    document.getElementById('#registro').addEventListener('click', function(){
+    document.getElementById('registro').addEventListener('click', function(){
         window.location.href = "signIn.html";
     });
 }
 
 function Login(){
-    document.getElementById('#iniciarSesion').addEventListener('click', function(){
+    document.getElementById('iniciarSesion').addEventListener('click', function(){
         window.location.href = "login.html";
     });
 }
