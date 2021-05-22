31f18a206fcfe9b1f00dbe5c351e3449cdb78cb3
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-05-22 18:41:03 -0500

    *Added main.js*

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+function SignIn(){
+    document.querySelector('#registro').addEventListener('click', function(){
+        window.location.href = "signIn.html";
+    });
+}
+
+function Login(){
+    document.querySelector('#iniciarSesion').addEventListener('click', function(){
+        window.location.href = "login.html";
+    });
+}
