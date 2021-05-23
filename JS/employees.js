fb05089fc66200e74c37e82204e911cca259f6a3
Jorge Bernal <jorgealejandrobernal@gmail.com>
2021-05-23 01:13:21 -0500

    *Updated employees.js*

--- JS/employees.js
+++ JS/employees.js
@@ -27,18 +27,46 @@ function loadEmployee(){
 
 function displayEmployee(employees) {
     var body = document.getElementsByTagName("body")[0];
-  
     var tabla = document.createElement("table");
     var tblBody = document.createElement("tbody");
-    var atributos = [`employee_id`, `employee_name`, `last_name`, `mail`, `pass`, `phone_num`, `address`];
     for (var i = 0; i < employees.length; i++) {
       var hilera = document.createElement("tr");
-      for (var j = 0; j < atributos.length; j++) {
-        var celda = document.createElement("td");
-        var textoCelda = document.createTextNode(`${employees[i].atributos[j]}`);
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-      }
+
+          var celdaId = document.createElement("td");
+        var textoId = document.createTextNode(`${employees[i].employee_id}`);
+        celdaId.appendChild(textoId);
+        hilera.appendChild(celdaId);
+      
+        var celdaEmployee_name = document.createElement("td");
+        var textoEmployee_name = document.createTextNode(`${employees[i].employee_name}`);
+        celdaEmployee_name.appendChild(textoEmployee_name);
+        hilera.appendChild(celdaEmployee_name);
+
+        var celdaLast_name = document.createElement("td");
+        var textoLast_name = document.createTextNode(`${employees[i].last_name}`);
+        celdaLast_name.appendChild(textoLast_name);
+        hilera.appendChild(celdaLast_name);
+
+        var celdaMail = document.createElement("td");
+        var textoMail = document.createTextNode(`${employees[i].mail}`);
+        celdaMail.appendChild(textoMail);
+        hilera.appendChild(celdaMail);
+
+        var celdaPass = document.createElement("td");
+        var textoPass = document.createTextNode(`${employees[i].pass}`);
+        celdaPass.appendChild(textoPass);
+        hilera.appendChild(celdaPass);
+
+        var celdaPhone_num = document.createElement("td");
+        var textoPhone_num = document.createTextNode(`${employees[i].phone_num}`);
+        celdaPhone_num.appendChild(textoPhone_num);
+        hilera.appendChild(celdaPhone_num);
+
+        var celdaAddress = document.createElement("td");
+        var textoAddress = document.createTextNode(`${employees[i].address}`);
+        celdaAddress.appendChild(textoAddress);
+        hilera.appendChild(celdaAddress);
+
       tblBody.appendChild(hilera);
     }
     tabla.appendChild(tblBody);
